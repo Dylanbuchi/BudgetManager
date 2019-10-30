5176f873249d7f297e97c638500f8e43366b1a81
Dylan <devybuchi@gmail.com>
2019-10-30 10:55:29 -0300

    code refactoring

--- src/app/Menu.java
+++ src/app/Menu.java
@@ -4,23 +4,16 @@ package app;
  * ShowMenu
  */
 public class Menu extends Items {
+    String userChoice;
 
     public Menu() {
-
+        userChoice = "_";
     }
 
     public void menu() {
 
-        String userChoice = "_";
-
         while (!userChoice.equals("0")) {
-
-            System.out.println("Choose your action: ");
-            System.out.println("1) Add income ");
-            System.out.println("2) Add purchase ");
-            System.out.println("3) Show list of purchases ");
-            System.out.println("4) Balance ");
-            System.out.println("0) Exit ");
+            showMenu();
 
             userChoice = in.nextLine();
 
@@ -31,41 +24,21 @@ public class Menu extends Items {
                 break;
 
             case "1":
-                System.out.print("\nEnter income: \n");
-                userChoice = in.nextLine();
-
-                setBalance(Double.parseDouble(userChoice));
-
-                System.out.println("\nIncome was added!\n");
-
+                addIncome();
                 break;
+
             case "2":
-                System.out.print("\nEnter purchase name: \n");
-                userChoice = in.nextLine();
-                String key = userChoice;
-
-                System.out.print("\nEnter its price:\n ");
-                userChoice = in.nextLine();
-                double value = Double.parseDouble(userChoice);
-
-                list.put(key + " $", value);
-
-                setList(list);
-                System.out.println("\nPurchase was added!\n");
-
+                addPurchase();
                 break;
+
             case "3":
-                if (getList().isEmpty()) {
-                    System.out.println("\nPurchase list is empty\n");
-
-                } else {
-                    System.out.println("\n" + getList().toString().replaceAll("[{=}]", "").replace("", "") + "\n");
-                }
-                System.out.println("\nTotal sum: $" + sumList() + "\n");
+                showListOfItems();
                 break;
+
             case "4":
                 showBalance();
                 break;
+
             default:
                 System.out.println("\nPlease put a valid number (0 - 4) to pick an item from the list\n ");
                 break;
@@ -85,4 +58,52 @@ public class Menu extends Items {
 
     }
 
+    public void addIncome() {
+
+        System.out.print("\nEnter income: \n");
+        userChoice = in.nextLine();
+
+        setBalance(Double.parseDouble(userChoice));
+
+        System.out.println("\nIncome was added!\n");
+
+    }
+
+    public void addPurchase() {
+
+        System.out.print("\nEnter purchase name: \n");
+        userChoice = in.nextLine();
+        String key = userChoice;
+
+        System.out.print("\nEnter its price:\n ");
+        userChoice = in.nextLine();
+        double value = Double.parseDouble(userChoice);
+
+        list.put(key + " $", value);
+        setList(list);
+        balance -= value;
+        System.out.println("\nPurchase was added!\n");
+
+    }
+
+    public void showListOfItems() {
+        if (getList().isEmpty()) {
+            System.out.println("\nPurchase list is empty\n");
+
+        } else {
+            System.out.println("\n" + getList().toString().replaceAll("[{=}]", "").replace("", "") + "\n");
+        }
+        System.out.println("\nTotal sum: $" + sumList() + "\n");
+
+    }
+
+    public void showMenu() {
+        System.out.println("Choose your action: ");
+        System.out.println("1) Add income ");
+        System.out.println("2) Add purchase ");
+        System.out.println("3) Show list of purchases ");
+        System.out.println("4) Balance ");
+        System.out.println("0) Exit ");
+
+    }
 }
